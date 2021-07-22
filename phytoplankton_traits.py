f8762345f0d4aad03c749959c45065cba2dde24b
Jurg Spaak <j.w.spaak@gmail.com>
2021-07-22 14:04:41 -0400

    Change generation of phytoplankton traits
    Include a correlation matrix
    Compare allometric scaling to empircally measured correlation
    Allow setting correlation to different values

--- phytoplankton_traits.py
+++ phytoplankton_traits.py
@@ -3,6 +3,43 @@ import pandas as pd
 import warnings
 from scipy.stats import linregress
 
+def proj_cov(A, tol = 1e-10, max_iter = 100):
+    """
+    Reference:  N. J. Higham, Computing the nearest correlation
+    matrix---A problem from finance. IMA J. Numer. Anal.,
+    22(3):329-343, 2002.
+    """
+
+    dx = np.zeros(np.shape(A))
+    # x projected onto covariance matrix space
+    X_spd = np.copy(A)
+    # relative difference between last and this iteration
+    rel_diff = np.inf
+    iteration = 1
+    while rel_diff > tol:
+        iteration +=1
+        # avoid infinite loop
+        if iteration > max_iter:
+            raise RuntimeError
+        # copy old value
+        X_spd_old = np.copy(X_spd)
+        
+        # move away from covariance matrices
+        X_off = X_spd - dx
+        
+        # project onto smi positive definite amtrices
+        eigval, eigvec = np.linalg.eigh(X_off)
+        X_spd = (eigvec*np.maximum(eigval,0)).dot(eigvec.T)
+        
+        # find next step length
+        dx = X_spd - X_off
+        
+        # project onto covariance matrices
+        np.fill_diagonal(X_spd, 1)
+        rel_diff = np.linalg.norm(X_spd - X_spd_old)/np.linalg.norm(X_spd)
+        
+    return X_spd
+
 # traits relevant for pyhtoplankton growth rates
 phyto_traits = np.array(["size_P", "mu_P", "k_n", "k_p", "k_l",
                          "c_n", "c_p", "a","e_P", "R_P"])
@@ -14,6 +51,7 @@ allo_scal = {} # allometric scaling parameters
 # gaussians stores log normal distribution
 A_phyto = pd.DataFrame(np.full((len(phyto_traits), len(phyto_traits)), np.nan),
                        index = phyto_traits, columns = phyto_traits)
+
 
 
 ##############################################################################
@@ -84,15 +122,19 @@ raw_data = raw_data.append(defense_data, ignore_index=True)
 # phytoplankton absorption coefficients
 augusti = pd.read_csv("empirical_data/augusti_data.csv")
 augusti = augusti[["d", "a"]]
-augusti["d"] = augusti["d"]**3 # convert diameter to volume
 augusti["a"] = augusti["a"]*1e-6 # convert \mum^2 to mm^2
+del augusti["d"]
 augusti = np.log(augusti)
 
 raw_data = raw_data.append(augusti, ignore_index=True)
 allo_scal["a"] = 0.77
 ###############################################################################
 mean_phyto = pd.DataFrame(columns = phyto_traits, index = [1])
-
+std_phyto = pd.DataFrame(columns = phyto_traits, index = [1])
+corr_theory = pd.DataFrame(np.full((len(phyto_traits), len(phyto_traits)), np.nan),
+                       index = phyto_traits, columns = phyto_traits)
+n_measurments = pd.DataFrame(np.full((len(phyto_traits), len(phyto_traits)), np.nan),
+                       index = phyto_traits, columns = phyto_traits)
 # remove outliers and fit one dimensional trait data
 for i,trait in enumerate(phyto_traits):
     # remove outliers
@@ -104,6 +146,8 @@ for i,trait in enumerate(phyto_traits):
     
     mean_phyto[trait] = np.nanmean(raw_data[trait])
     A_phyto.loc[trait, trait] = np.nanvar(raw_data[trait])
+    std_phyto[trait] = np.nanstd(raw_data[trait])
+    
     
 def nan_linreg(x,y):
     x,y = raw_data[x].values, raw_data[y].values
@@ -113,44 +157,48 @@ def nan_linreg(x,y):
     return linregress(x[ind], y[ind])
 
 
-size_var = np.nanvar(raw_data["size_P"]) 
+size_var = np.nanvar(raw_data["size_P"])
+size_std = np.nanstd(raw_data["size_P"])
 s, i, r, p, std = nan_linreg("mu_P", "k_l")
 allo_scal["k_l"] = allo_scal["mu_P"]*s
 allo_scal["e_P"] = nan_linreg("size_P", "e_P")[0]
 
 for i,trait in enumerate(phyto_traits):
+    corr_theory.loc[trait, trait] = 1
     for j, traitj in enumerate(phyto_traits):
         if i != j: # different traits
             A_phyto.loc[trait, traitj] = (allo_scal[trait]*allo_scal[traitj]
                                          *size_var)
-            
-# compare to correlations in data
-tr_norm = raw_data.values - np.nanmean(raw_data.values, axis = 0)
-with warnings.catch_warnings(record = True):
-    A_phyto_measured = np.nanmean(tr_norm.T[...,np.newaxis]*tr_norm, axis = 1)
+            corr_theory.loc[trait, traitj] = (allo_scal[trait]*allo_scal[traitj]
+                        *size_var
+                        /(std_phyto[trait]*std_phyto[traitj])).values
+            n_measurments.loc[trait, traitj] = np.sum(np.isfinite(
+                                        raw_data[trait]*raw_data[traitj]))
+with warnings.catch_warnings(record = True):           
+    # compute confidence interval of correlation based on empirical measurments
+    corr_empirical = raw_data.corr()
+    # convert to fisher z'
+    fisher_z = np.arctanh(corr_empirical.values)
+    # confidence interval
+    alpha = 1.96 # 95% confidence 
+    alpha = np.array([-alpha, alpha]).reshape(-1,1,1)
+    fisher_confidence = fisher_z + alpha/np.sqrt(n_measurments.values - 3)
+    corr_confidence = np.tanh(fisher_confidence)
+    
+    # where not in confidence interval
+    corr_phyto = corr_theory.copy()
+    ind = (corr_phyto<corr_confidence[0]) | (corr_phyto>corr_confidence[1])
+    corr_phyto[ind] = corr_empirical
+    
+    
+# this matrix might not be positive semidefinite (i.e. not a covariance matrix)
+# find the closest covariance matrix (measured in Frobenius norm)
+corr_phyto = proj_cov(corr_phyto)    
+
+# the base covariance matrix of phytoplankton
+cov_phyto = corr_phyto*std_phyto.values*std_phyto.values[0,:,np.newaxis]
 ##############################################################################
 # find parameters for scaling size to traits
-"""
-#scaling factors for size, minimum value
-# data from doi:10.1093/plankt/fbp098, Finkel et al 2010
-gaussians = gaussians.append({"mu_P": -0.25, "k_p": 0.5, "k_n": 0.5,
-                              "c_n": 2/3, "c_p": 2/3,
-                              "a": 0.77, "size_P": 1,
-                              "k_l": cor_mul_kl*(-0.25),
-                              "R_P": 0.8}, ignore_index = True)
-
-
-# add allometric scaling information
-gaussians = gaussians.T
-gaussians.columns = ["mean_trait", "std_trait", "beta_min"]
-
-# size disribution mean and standarddeviation
-mean_size = np.nanmean(phyto_traits["size_P"])
-std_size = np.nanmin(np.abs(gaussians["std_trait"]/gaussians["beta_min"]))
-std_size = np.nanstd(raw_data["size_P"])
-"""
-
-
 # add certain tradeoffs
 A_tradeoff = pd.read_csv("empirical_data/Three_way_tradeoff.csv", index_col=0)
 cov_tradeoff = A_phyto.copy()
@@ -159,11 +207,44 @@ for traiti in A_tradeoff.columns:
     for traitj in A_tradeoff.columns:
         cov_tradeoff.loc[traiti, traitj] = A_tradeoff.loc[traiti, traitj]
     
-def generate_phytoplankton_traits(r_spec = 1, n_com = 100):
+def generate_phytoplankton_traits(r_spec = 1, n_com = 100,
+                                  std = None,
+                                  corr = None,
+                                  const_traits = {}, tradeoffs = {}):
+    
+    if std is None:
+        std = std_phyto.copy()
+    if corr is None:
+        corr = corr_phyto.copy()
+    
+    # change variation of certain traits
+    for key in const_traits.keys():
+        std[key] = const_traits[key]
+    
+    # change tradeoffs between traits
+    for key in tradeoffs.keys():
+        key1, key2 = key.split(":")
+        corr.loc[key1, key2] = tradeoffs[key]
+        corr.loc[key2, key1] = tradeoffs[key]    
+    
+    # combine corrlation and standard deviation into covariance matrix
+    A_phyto = corr*std.values*std.values[0,:,np.newaxis]
+    
+    # covariance matrix for meta community
+    A_meta_com = cov_phyto - A_phyto
+    
     # generate multivariate distribution
-    traits = np.exp(np.random.multivariate_normal(mean_phyto.values[0],
-                                                  A_phyto,
-                                                  (n_com, r_spec)))
+    # mean trait values per community
+    com_mean = np.random.multivariate_normal(mean_phyto.values[0],
+                                      A_meta_com, (n_com, 1))
+    # variation around community mean
+    species_differences = np.random.multivariate_normal(
+                                        np.zeros(len(A_meta_com)),
+                                      A_phyto, (n_com, r_spec))
+    
+    # traits are community man + species differences
+    traits = np.exp(com_mean + species_differences)
+    
     # order species according to their size
     order = np.argsort(traits[...,0], axis = 1)
     trait_dict = {}
@@ -172,7 +253,7 @@ def generate_phytoplankton_traits(r_spec = 1, n_com = 100):
     
     return trait_dict
 
-if __name__ == "__main__":
+if __name__ == "__main__" and False:
     import matplotlib.pyplot as plt
     
     traits = generate_phytoplankton_traits(10,100)
